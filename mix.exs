a0a12fc1319cd2b4454a4b57626b94077eab933e
hchan-enbala <hchan@enbala.com>
2016-11-24 14:57:39 -0800

    moved helper functions to its own module

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule HW.Mixfile do
+defmodule SYMPHONY_QA.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :hw,
+    [app: :symphony_qa,
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
